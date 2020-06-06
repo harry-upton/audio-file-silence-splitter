f3df6aad6abcb1adbe381905162f4521627976c5
Harry Upton <harryupton100@gmail.com>
2020-06-06 13:37:33 +0100

    Update AudioFileSilenceSplitter.py
    Removed unused code that wasn't deleted in last commit.

--- AudioFileSilenceSplitter.py
+++ AudioFileSilenceSplitter.py
@@ -3,27 +3,6 @@
 from pydub import AudioSegment, silence
 import argparse
 
-# Parse the arguments from the command line.
-parser = argparse.ArgumentParser()
-
-parser.add_argument("file", type=str, help="The name/path of the audio file(must be in this directory), eg. 'songs.mp3'")
-parser.add_argument("format", type=str, help="The format of the input audio file, eg. 'wav' or 'mp3'. mp3 requires ffmpeg.")
-parser.add_argument("-f", "--output_format",type=str, help="The format of the input audio file, eg. 'wav' or 'mp3'. mp3 requires ffmpeg", default="mp3")
-parser.add_argument("-t", "--threshold", type=int, help="The threshold volume for a chunk to be considered silent, in dBFS. You may want to adjust this parameter if your audio file is quieter than the threshold silence. Default=-16", default=-16)
-parser.add_argument("-l", "--min_silence_length", type=int, help="The minimum silence length (in ms), that a chunk must be for a split. Default=2000", default=2000)
-parser.add_argument("-p", "--padding", type=int, help="The silence to keep either side of a chunk (in ms), so the clip doesn't end too abruptly. Default=250", default=250)
-parser.add_argument("-b", "--bitrate", type=str, help="The bitrate of the output file. Default='192k'", default="192k")
-
-args = parser.parse_args()
-
-name = args.file
-inputFormat = args.format
-outputFormat = args.output_format
-threshold = args.threshold
-minSilenceLength = args.min_silence_length
-padding = args.padding
-br = args.bitrate
-
 def Main(name, inputFormat, outputFormat, threshold, minSilenceLength, padding, br):
     # Make an audio segment object from the audio file.
     print("Creating audio segment object...")
